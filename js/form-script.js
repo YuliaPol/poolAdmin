7a54c58cf75a8cea6532aed09cbc2071f760b928
YuliaPol <62880025+YuliaPol@users.noreply.github.com>
2021-12-13 11:08:19 +0200

    chnage create pool

--- js/form-script.js
+++ js/form-script.js
@@ -21,25 +21,58 @@ jQuery(function ($) {
             if (e.target.files && e.target.files[0]) {
                 let fileHtml = 
                 '<div class="file-item">'
-                +'    <div class="btn-delete">'
-                +'    </div>'
                 +'    <div class="file-icon"></div>'
                 +'</div>';
                 $(this).parents('.form-group').find('.files-container').html(fileHtml);
             }
-            $(this).parents('.input-file').find('label').addClass('active');
+            $('.file-popup').find('.file-icon').addClass('active');
         });
-        $('.create-pool-wrap').on('click', '.files-container .btn-delete', function(e){
-            let formGroup = $(this).parents('.form-group');
-            $(this).parents('.file-item').remove();
+        $('.file-popup').on('click', '.exel-delete', function(e){
+            let exelGroup = $('.input-exel');
+            let formGroup = exelGroup.parents('.form-group');
+            formGroup.find('.files-container .file-item').remove();
             let inputName = $(this).parents('.form-group').find('input[type=file]').attr('name');
             let accpetFiles = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel";
             formGroup.find('.add-pool-files').remove();
             let newInput = '<input type="file" name="' + inputName + '" class="add-pool-files" accept="' + accpetFiles + '">'
             formGroup.find('label').append(newInput);
-            formGroup.find('label').removeClass('active');
+            $('.file-popup').find('.file-icon').removeClass('active');
         });
 
+        $('.create-pool-wrap').on('change', '.add-exel', function(e){
+            if($(this).is(':checked')){
+                $('.file-popup').fadeIn(300);
+            } else {
+                if($('.file-popup').find('.file-icon').hasClass('active')){
+                    $('.file-popup').find('.exel-delete').click();
+                }
+            }
+        });
+        $('.create-pool-wrap').on('click', '.files-container .file-item', function(e){
+            $('.file-popup').fadeIn(300);
+        });
+        $('.file-popup').on('click', '.file-icon', function(e){
+            if(!$(e.target).hasClass('exel-delete')){
+                $('.create-pool-wrap').find('.input-exel').find('input[type=file]').click();
+            }
+        });
+        $('.popup-wrap').on('click', '.close-popup', function(e){
+            $(this).parents('.popup-wrap').fadeOut();
+            if($(this).parents('.popup-wrap').hasClass('file-popup')){
+                beforeHideFilePopup();
+            }
+        });
+        function beforeHideFilePopup(){
+            if(!$('.file-popup').find('.file-icon').hasClass('active')){
+                $('.create-pool-wrap').find('.add-exel').prop('checked', false);
+            }
+        }
+        $('.popup-wrap').click(function(e){
+            if($(e.target).parents('.popup-wrap').length === 0){
+                $(this).fadeOut(300);
+                beforeHideFilePopup();
+            }
+        });
         //validation
         var formValid = document.getElementsByClassName('form-valid')[0];
         $('.send-form').click(function () {
